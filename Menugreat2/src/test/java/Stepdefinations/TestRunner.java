652cbb2a24127ff57e0ac91f6c9bf5b880a477c9
Md Mahedi Hasan <30hasanmahedi@gmail.com>
2020-12-21 13:55:24 +0600

    Logout feature is added

--- Menugreat2/src/test/java/Stepdefinations/TestRunner.java
+++ Menugreat2/src/test/java/Stepdefinations/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features/test.feature",glue= {"Stepdefinations"})
+@CucumberOptions(features="src/test/resources/Features/test1.feature",glue= {"Stepdefinations"})
  
 public class TestRunner {
 
